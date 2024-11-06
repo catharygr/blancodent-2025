ff0721ef9b6575d90d49091954f16ae8a550b78d
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-06 11:53:53 +0000

    Terminada la instalación y configuración de i18n && git checkout main && git merge i18n && git checkout i18n && git log

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+// i18n plugin
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// Exportado por defecto por Netx.js
+// export default nextConfig;
+
+// Exportando el plugin con i18n plugin
+export default withNextIntl(nextConfig);
