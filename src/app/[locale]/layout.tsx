b41b9eeb22768e8ac7f93d33757b16c6aefd3268
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-07 12:35:48 +0000

    Trabajando en robots.ts y sitemap.ts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,21 +7,18 @@ import {
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
-// export const metadata: Metadata = {
-//   title: "Clínica Dental Blancodent - Tus dentistas en Las Palmas",
-//   description:
-//     "Clínica dental en Las Palmas, BLANCODENT. Dentistas en Las Palmas. Dentistas en Mesa y López. Expertos en ortodoncia, endodoncia, tratamiento del bruxismo, periodoncia e implantología dental. Puedes encontrar todas las especialidades odontológicas en BLANCODENT.",
-// };
 // Generar las rutas estáticas para cada idioma - i18n
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
-
+// Generar los metadatos para cada idioma - i18n
 export async function generateMetadata({
-  params: { locale },
+  params,
 }: {
   params: { locale: string };
 }) {
+  const { locale } = await params;
+
   const t = await getTranslations({ locale, namespace: "Metadata" });
 
   return {
