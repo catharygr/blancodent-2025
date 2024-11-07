23f257c2770029d51b355e884b9d467444748433
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-07 10:49:00 +0000

    Trabajando en el documento not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import Error from "next/error";
+import Link from "next/link";
+
+const styles = {
+  a: {
+    display: "inline-block",
+    padding: "1rem 0 0 1rem",
+    color: "white",
+    textDecoration: "underline",
+    textUnderlineOffset: "0.4em",
+  },
+};
 
 export default function NotFound() {
   return (
     <html lang="en">
       <body>
+        <Link
+          style={styles.a}
+          href="/"
+        >
+          Home
+        </Link>
         <Error statusCode={404} />
       </body>
     </html>
