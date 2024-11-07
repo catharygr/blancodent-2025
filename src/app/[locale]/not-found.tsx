423322263e76db657d2e3513aa358772b5e2cee8
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-07 11:07:35 +0000

    Trabajando en el  css de not-found.tsx

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,6 +1,24 @@
 import { useTranslations } from "next-intl";
 
+import { CSSProperties } from "react";
+import { Link } from "@/i18n/routing";
+
+const styles: { container: CSSProperties } = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  },
+};
+
 export default function NotFoundPage() {
   const t = useTranslations("NotFoundPage");
-  return <h1>{t("title")}</h1>;
+  return (
+    <div style={styles.container}>
+      <h1>{t("title")}</h1>
+      <Link href="/">{t("homeLink")}</Link>
+    </div>
+  );
 }
