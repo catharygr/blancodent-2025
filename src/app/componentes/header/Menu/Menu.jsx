f86fe812dd772e040aafdcc284852907e7ec4f13
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-26 12:02:19 +0000

    Trabajando con MenuDrawer.module.css

--- src/app/componentes/header/Menu/Menu.jsx
+++ src/app/componentes/header/Menu/Menu.jsx
@@ -1,13 +1,25 @@
 "use client";
 import styles from "./Menu.module.css";
 import { List } from "@phosphor-icons/react/dist/ssr";
+import MenuDrawer from "../MenuDrawer/MenuDrawer";
+import { useState } from "react";
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.container}>
-      <button className={styles.btn}>
+      <button
+        className={styles.btn}
+        onClick={handleOpenDrawer}
+      >
         <List weight="bold" />
       </button>
+      {isOpen && <MenuDrawer />}
     </nav>
   );
 }
