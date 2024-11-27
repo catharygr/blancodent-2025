c72894d92fc6ea75b583dc02ea00541f220290c3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-27 11:44:07 +0000

    Trabajando en MenuDrawer.module.css

--- src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
+++ src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
@@ -1,11 +1,14 @@
 import styles from "./MenuDrawer.module.css";
 import { useEffect } from "react";
+import { EyeClosed } from "@phosphor-icons/react/dist/ssr";
+import { useTranslations } from "next-intl";
 
 interface MenuDrawerProps {
   handleCloseDrawer: () => void;
 }
 
 export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
+  const t = useTranslations("MainMenu");
   useEffect(() => {
     const elementoEnfocadoAntesDeAbrirlo = document.activeElement;
     return () => {
@@ -26,7 +29,10 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
   }, [handleCloseDrawer]);
   return (
     <div className={styles.navContainer}>
-      <div className={styles.navBackground} />
+      <div
+        onClick={handleCloseDrawer}
+        className={styles.navBackground}
+      />
       <div className={styles.navDrawer}>
         <div className={styles.navLinks}>
           <a href="#">Home</a>
@@ -35,7 +41,11 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
           onClick={handleCloseDrawer}
           className={styles.closeBtn}
         >
-          X
+          <EyeClosed
+            aria-hidden="true"
+            weight="bold"
+          />
+          {t("closeMenuBtn")}
         </button>
       </div>
     </div>
