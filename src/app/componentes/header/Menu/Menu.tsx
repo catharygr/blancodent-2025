bd8da16e0a5aa462b57291c171d6c370be8eb944
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-27 10:32:44 +0000

    Trabajando con el hook useTranslations()

--- src/app/componentes/header/Menu/Menu.tsx
+++ src/app/componentes/header/Menu/Menu.tsx
@@ -3,16 +3,21 @@ import styles from "./Menu.module.css";
 import { List } from "@phosphor-icons/react/dist/ssr";
 import MenuDrawer from "../MenuDrawer/MenuDrawer";
 import { useState } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Menu() {
   const [isOpen, setIsOpen] = useState(false);
+  const t = useTranslations("MainMenu");
 
   const handleOpenDrawer = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className={styles.container}>
+    <nav
+      className={styles.container}
+      aria-label="navLabel"
+    >
       <button
         className={styles.btn}
         onClick={handleOpenDrawer}
