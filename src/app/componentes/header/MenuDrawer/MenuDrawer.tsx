e78798596429070825d810c7183c6095c48124cb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-29 14:14:00 +0000

    Trabajando en MenuDrawer.module.css

--- src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
+++ src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
@@ -1,6 +1,6 @@
 import styles from "./MenuDrawer.module.css";
 import { useEffect } from "react";
-import { EyeClosed } from "@phosphor-icons/react/dist/ssr";
+import { X } from "@phosphor-icons/react/dist/ssr";
 import { useTranslations } from "next-intl";
 import FocusLock from "react-focus-lock";
 import { RemoveScroll } from "react-remove-scroll";
@@ -32,52 +32,65 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
       <FocusLock returnFocus={true}>
         <RemoveScroll>
           <div className={styles.navDrawer}>
-            <div
-              className={styles.accordionGroup}
-              role="group"
-              aria-labelledby="nav-links-group-label"
-            >
-              <span
-                id="nav-links-group-label"
-                className="visually-hidden"
-              >
-                {t("disclosureGroupLabel")}
-              </span>
-              <details className={styles.details}>
-                <summary className={styles.summary}>Nuestro servicio</summary>
-                <ul role="list">
-                  <li>Implantologia</li>
-                  <li>Ortodoncia</li>
-                  <li>Estética dental</li>
-                </ul>
-              </details>
-              <details className={styles.details}>
-                <summary className={styles.summary}>Sobre nosotros</summary>
-                <ul role="list">
-                  <li>Implantologia</li>
-                  <li>Ortodoncia</li>
-                  <li>Estética dental</li>
-                </ul>
-              </details>
-              <details className={styles.details}>
-                <summary className={styles.summary}>Articulos</summary>
-                <ul role="list">
-                  <li>Implantologia</li>
-                  <li>Ortodoncia</li>
-                  <li>Estética dental</li>
-                </ul>
-              </details>
-            </div>
             <button
               onClick={handleCloseDrawer}
               className={styles.closeBtn}
+              aria-labelledby="close-btn-label"
             >
-              <EyeClosed
+              <span
+                className="visually-hidden"
+                id="close-btn-label"
+              >
+                {t("closeMenuBtn")}
+              </span>
+              <X
                 aria-hidden="true"
                 weight="bold"
               />
-              {t("closeMenuBtn")}
             </button>
+            <div className={styles.innerContainer}>
+              <ul role="list">
+                <li>Implantologia</li>
+                <li>Ortodoncia</li>
+                <li>Estética dental</li>
+              </ul>
+              <div
+                className={styles.accordionGroup}
+                role="group"
+                aria-labelledby="nav-links-group-label"
+              >
+                <span
+                  id="nav-links-group-label"
+                  className="visually-hidden"
+                >
+                  {t("disclosureGroupLabel")}
+                </span>
+                <details className={styles.details}>
+                  <summary className={styles.summary}>Nuestro servicio</summary>
+                  <ul role="list">
+                    <li>Implantologia</li>
+                    <li>Ortodoncia</li>
+                    <li>Estética dental</li>
+                  </ul>
+                </details>
+                <details className={styles.details}>
+                  <summary className={styles.summary}>Sobre nosotros</summary>
+                  <ul role="list">
+                    <li>Implantologia</li>
+                    <li>Ortodoncia</li>
+                    <li>Estética dental</li>
+                  </ul>
+                </details>
+                <details className={styles.details}>
+                  <summary className={styles.summary}>Articulos</summary>
+                  <ul role="list">
+                    <li>Implantologia</li>
+                    <li>Ortodoncia</li>
+                    <li>Estética dental</li>
+                  </ul>
+                </details>
+              </div>
+            </div>
           </div>
         </RemoveScroll>
       </FocusLock>
