5371795aa15c07f672fbe000067d2155b342deac
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-03 12:00:39 +0000

    Cambio de grid a flex en el componente MenuDrawe.tsx

--- src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
+++ src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
@@ -59,18 +59,18 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
                 weight="bold"
               />
             </button>
+            <ul
+              className={styles.quickLinks}
+              role="list"
+            >
+              <li>
+                <Link href="/contact">Cita previa</Link> •
+              </li>
+              <li>
+                <Link href="tel:+34928292455">Llámanos</Link>
+              </li>
+            </ul>
             <div className={styles.innerContainer}>
-              <ul
-                className={styles.quickLinks}
-                role="list"
-              >
-                <li>
-                  <Link href="/contact">Cita previa</Link> •
-                </li>
-                <li>
-                  <Link href="tel:+34928292455">Llámanos</Link>
-                </li>
-              </ul>
               <div
                 className={styles.accordionGroup}
                 role="group"
@@ -140,10 +140,6 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
               <div className={styles.location}>
                 <h2>Dónde encontrarnos</h2>
                 <address>
-                  <Image
-                    src={smallMap}
-                    alt="Mapa de la ubicación de la clínica"
-                  />
                   <p>
                     Address:
                     <br />
@@ -155,6 +151,53 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
                     <br />
                   </p>
                 </address>
+                <Image
+                  src={smallMap}
+                  alt="Mapa de la ubicación de la clínica"
+                />
+                <Link
+                  target="_blank"
+                  href="https://www.google.com/maps/place/Cl%C3%ADnica+Dental+Blancodent/@28.134669,-15.429525,17z/data=!4m2!3m1!1s0x0:0x9293d54eccbd51c?hl=en"
+                >
+                  Abrir en el mapa de Google
+                </Link>
+                <br />
+                <Link
+                  target="_blank"
+                  href="https://maps.apple.com/maps?address=Av%20Mesa%20y%20L%C3%B3pez,%201%20-%201%C2%AA%20izquierda,%2035006%20Las%20Palmas%20de%20Gran%20Canaria,%20Las%20Palmas,%20Las%20Palmas,%20Spain&auid=4569860911891010439&ll=28.134679,-15.429491&lsp=9902&q=Cl%C3%ADnica%20Dental%20Blancodent&t=m"
+                >
+                  Abrir en el mapa de Apple
+                </Link>
+              </div>
+              <div className={styles.info}>
+                <div className={styles.contact}>
+                  <h2>¿Cómo contactar con nosotros?</h2>
+                  <br />
+                  <p>
+                    Puedes solicitar cualquier pregunta y hacer cita en nuestro
+                    formulario de contacto.
+                  </p>
+                  <br />
+                  <p>
+                    Puedes llamarnos en nuesyro horario de apertura o puedes
+                    dejarnos un mensaje aquí: LLámanos
+                  </p>
+                </div>
+                <br />
+                <div className={styles.timetable}>
+                  <h2>Horarios</h2>
+                  <p>
+                    Lunes: 09:30h - 16:00h
+                    <br />
+                    Martes: 14:00 - 20:00h
+                    <br />
+                    Miércoles: 09:30h - 16:00h
+                    <br />
+                    Jueves: 14:00 - 20:00h
+                    <br />
+                    Viernes: 09:30h - 16:00h
+                  </p>
+                </div>
               </div>
             </div>
           </div>
