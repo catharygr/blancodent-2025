d2785334248a5565fe02008b52482da1542c1106
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-09 17:04:48 +0000

    Trabajando con framer motion

--- src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
+++ src/app/componentes/header/MenuDrawer/MenuDrawer.tsx
@@ -41,9 +41,8 @@ export default function MenuDrawer({ handleCloseDrawer }: MenuDrawerProps) {
             animate={{ y: "110%" }}
             transition={{
               type: "spring",
-              stiffness: 300,
-              damping: 25,
-              duration: 0.5,
+              stiffness: 150,
+              damping: 20,
             }}
             exit={{ y: "0%" }}
           >
