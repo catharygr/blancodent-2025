adebd6f802744615a1535a834e4d855d98d5489f
catharygr <103951447+catharygr@users.noreply.github.com>
2025-12-15 13:17:13 +0100

    Trabajando en el CSS del componente Opinions

--- src/components/HomePage/Opinions/Opinions.tsx
+++ src/components/HomePage/Opinions/Opinions.tsx
@@ -4,12 +4,37 @@ import { getTranslations } from "next-intl/server";
 export default async function Opinions() {
   const t = await getTranslations("HomePage.opinions");
   return (
-    <section className={`${styles.container} narrow-grid`}>
+    <section className={styles.container}>
       <div>
-        <h2> {t("title")} </h2>
+        <h2 className={styles.opinionsTitle}> {t("title")} </h2>
         <div className={styles.decorativeLine} />
       </div>
-      <div className={styles.cardContainer}></div>
+      <div className={styles.opinionsContainer}>
+        <article className={styles.firstOpinionArticle}>
+          <header className={styles.firstOpinionHeader}>
+            <h3>Jordan</h3>
+            <span>*****</span>
+          </header>
+          <blockquote>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
+              nesciunt animi nihil sunt ad mollitia quasi unde omnis et error
+              voluptatum quas eveniet quibusdam laudantium explicabo debitis
+              velit, quia quos.
+            </p>
+          </blockquote>
+        </article>
+        <div className={styles.otherOpinionsContainer}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
     </section>
   );
 }
